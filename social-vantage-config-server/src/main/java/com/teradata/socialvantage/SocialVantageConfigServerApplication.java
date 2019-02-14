6a027a3e1002ec4f8025d705d7b24df43526be73
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-14 12:20:27 +0530

    added config server

---
+++ social-vantage-config-server/src/main/java/com/teradata/socialvantage/SocialVantageConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.teradata.socialvantage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialVantageConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialVantageConfigServerApplication.class, args);
+	}
+
+}
+
