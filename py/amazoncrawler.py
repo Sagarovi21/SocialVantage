8d6da28367f8343a5e37f1ee57bfc39001de6c90
Kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-19 13:03:23 +0530

    crawll

--- py/amazoncrawler.py
+++ py/amazoncrawler.py
@@ -13,60 +13,32 @@ import teradata
 import asyncio
 from multiprocessing import Pool
 
-app = Flask(__name__)
-api = Api(app)
-udaExec = teradata.UdaExec (appName="mkbasedb", version="1.0",logConsole=False)
-session = udaExec.connect(method="odbc", system="sdt20168.labs.teradata.com",username="cim", password="cim")
-urls=[]
-data = []
 class Comments(scrapy.Spider):
     name = "ScraperWithLimit"
-    loop = asyncio.get_event_loop()
-    tasks = []
-    
     def __init__(self, domain=None, commonUrls = [],task_id=None,data=None, *args, **kwargs):
-        commonUrls = set(commonUrls)
+        self.commonUrls = set(commonUrls)
         self.urls =commonUrls
         self.task_id = task_id
         self.data = data
         
 
     def start_requests(self):
-        count = 0
-        self.cmnts = 0
-        number_pages = str(len(urls))
-        session.execute("""UPDATE mkbasedb.TaskStatus SET pages_found = ?
-                                WHERE task = ?  and task_id= ? """,
-                             (number_pages, 'webcrawl',self.task_id))
-        session.commit()
+       
         for url in self.urls:
-            count += 1
+           
             try:
-                self.data = ()
-                session.execute("""UPDATE mkbasedb.TaskStatus SET pages_completed = ? , task_status = ?  WHERE task = ? and task_id = ? """,
-                                         (str(count), 'inprogress','webcrawl', self.task_id ))
-                session.commit()
-                yield scrapy.Request(url=url, callback=self.parse_phone_details)
-                
+                yield scrapy.Request(url=url, callback=self.parse_phone_details)  
             except Exception as error:
                 print('An error occured during page opening')
                 print(error)
-        
-        print("**********************************************"+ str(self.cmnts))
-        session.execute("""UPDATE mkbasedb.TaskStatus SET comments_found = ?, task_status = ?
-                                WHERE task = ? and task_id = ? """,
-                                (self.cmnts, 'done', 'webcrawl', self.task_id))
-        session.commit()
-        self.loop.run_until_complete(asyncio.wait(self.tasks))  
-        self.loop.close()
 
     def parse_phone_details(self, response):
-        start = time.time()
+        
         try:
             phone_name = response.xpath('//span[contains(@id,"productTitle")]/text()').extract_first().strip()
             product_details = response.xpath('//div[contains(@id,"prodDetails")]')
             technial_details = product_details.xpath('.//div[contains(@class,"pdTab")]')[0]
-            
+           
             feature_names = technial_details.xpath('.//td[contains(@class,"label")]/text()').extract()
             feature_values = technial_details.xpath('.//td[contains(@class,"value")]/text()').extract()
             features_data = dict(zip(feature_names,feature_values))
@@ -85,12 +57,7 @@ class Comments(scrapy.Spider):
             print('An error occured parse_phone_details')
             print(error)
 
-        end = time.time()
-        print(end - start)
-        print("#############################")
-
     def parse_phone_reviews(self,response):
-        phone_name = response.meta['phone_name']
         product_details = response.meta['product_details']
         if len (product_details) > 230 :
             product_details = product_details[:230]
@@ -103,6 +70,7 @@ class Comments(scrapy.Spider):
         feature_values = response.meta['feature_values']
         if len (feature_values) > 230 :
             feature_values = feature_values[:230]
+        phone_name = response.meta['phone_name']
         reviews = response.xpath('//div[contains(@id,"customer_review")]')
         count = 0
         for review in reviews:
@@ -119,8 +87,6 @@ class Comments(scrapy.Spider):
             data = (self.task_id,phone_name, review_text, rating,total_rating, product_details,technial_details,feature_names,  feature_values )
             print( "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
             print(data )
-            self.cmnts += 1
-            self.tasks.append(asyncio.ensure_future(self.runQuery(data)))
             if( count > 10):
                 break
             count += 1
@@ -128,57 +94,26 @@ class Comments(scrapy.Spider):
         absolute_next_page_url = response.urljoin(next_page_url)
         yield  Request(absolute_next_page_url,callback=self.parse_phone_reviews,meta={'phone_name':phone_name, 'product_details' : product_details,'technial_details':'test','feature_names' : feature_names,'feature_values':feature_values  })
 
-    async def runQuery(self,data):
-        try:
-            session.execute("""INSERT INTO mkbasedb.reviews (task_id, ObjectName, Review, Rating, TotalRating, product_details, technial_details,feature_names, feature_values )
-                        VALUES (?, ?, ?, ?, ?, ? , ? , ? , ?)""",
-                        data)
-            session.commit()
-            print(data)
-        except Exception as error:
-            print('An error occured run query')
-            print(error)
-            print(self.urls)
-
-    def taskQuery(self):
-        session.execute("""INSERT INTO mkbasedb.taskstatus (task_id, task, ObjectName, pages_found, pages_completed, comments_found, task_status)
-                        VALUES (?, ?, ?, ?, ?,?,?)""",
-                        (self.task_id, 'webcrawl', len(self.urls),0,0,0,'initiated'))
-        session.commit()
-
-
-
-@app.route('/search/<int:task_id>/<string:search_string>', methods=['GET'])
-def get_tasks(task_id,search_string):
-    pool = Pool(processes=1)
-    result = pool.apply_async(process, [task_id,search_string]) 
-    return jsonify({'task_id': task_id, 'search_string': search_string, 'status' : 'initiated'})
-
 
 def process(task_id, search_string):
     start = time.time()
 
     """app.run(port='5002')"""
     urls = Reader.readFile('https://www.amazon.in/s/ref=nb_sb_noss?field-keywords='+search_string)
-    
+
     logging.getLogger('scrapy').setLevel(logging.WARNING)
     logging.getLogger('scrapy').propagate = False
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
-
-    session.execute("""INSERT INTO mkbasedb.taskstatus (task_id, task, ObjectName, pages_found, pages_completed, comments_found, task_status)
-                        VALUES (?, ?, ?, ?, ?,?,?)""",
-                        (task_id, 'webcrawl', len(urls),0,0,0,'initiated'))
-    session.commit()
-
-    process.crawl(Comments, domain=["https://gadgets.ndtv.com/mobiles/smartphones"],commonUrls=urls, task_id=task_id, data = data)
+    process.crawl(Comments, domain=["https://gadgets.ndtv.com/mobiles/smartphones"],commonUrls=urls, task_id=task_id)
     process.start()
     
-
+    
     end = time.time()
     print(end - start)
     print("#############################")
 
+
 if __name__ == '__main__':
-        process(165,'smartphone')
+        process(124,'smartphone')
